4cbc3e6003c1acf9b28abff7e439fbcac9a02965
Dat <dattruong@jobhop.vn>
2021-06-06 22:55:21 +0700

    remove shared app

--- src/src/settings.py
+++ src/src/settings.py
@@ -46,13 +46,7 @@ SHARED_APPS = (
     'customer',  # you must list the app where your tenant model resides in
 
     # everything below here is optional
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
+   
     # 'fontawesome-free',
     "crispy_forms",
     "crispy_bootstrap5",
