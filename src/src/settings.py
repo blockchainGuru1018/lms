b9e3bf6ed38e5fda5edb2f83468322f81a548056
Dat <dattruong@jobhop.vn>
2021-06-20 20:12:21 +0700

    remove dup host user

--- src/src/settings.py
+++ src/src/settings.py
@@ -267,7 +267,7 @@ EMAIL_BACKEND = env.str(
     default='django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST_USER = env.str('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD', default='')
-EMAIL_HOST_USER = env.str('EMAIL_HOST_USER', default='')
+
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
 EMAIL_HOST_NAME = env.str('EMAIL_HOST_NAME', default='')
 
